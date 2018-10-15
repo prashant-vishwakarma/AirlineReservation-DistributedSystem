b15d4747869267391517da40837c188fc7915c4a
Prashant Vishwakarma <32397516+prashant-vishwakarma@users.noreply.github.com>
2018-10-15 17:34:52 +0530

    Update Server.java

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,16 +1,19 @@
-import java.io.DataInput;
+package ui;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class Server {
 
 	public static void main(String[] args) throws IOException {
 		
 		int clientCount = 0;
+		int ClientId = 0;
 		
 		ServerSocket serverSocket = new ServerSocket(1024);
 		while(true) {
@@ -18,10 +21,13 @@ public class Server {
 			try {
 				socket = serverSocket.accept();
 				System.out.println("Client Count: " + ++clientCount);
+				ClientId = 1000 + clientCount;
 				DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
 				DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+				ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+				ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 				System.out.println("Threaded Client #" + clientCount);
-				Thread thread = new ClientHandler(socket, dataInputStream, dataOutputStream);
+				Thread thread = new ClientHandler(socket, dataInputStream, dataOutputStream, objectInputStream, objectOutputStream, ClientId);
 				thread.start();
 			}catch (Exception e) {
 				socket.close();
