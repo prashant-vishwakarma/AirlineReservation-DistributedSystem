0d78705a6ac24d8816cbb492265aff0c2292e211
Prashant Vishwakarma <32397516+prashant-vishwakarma@users.noreply.github.com>
2018-10-24 22:55:14 +0530

    Update Server.java

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,34 +1,38 @@
-import java.io.DataInput;
+package ui;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class Server {
 
+	static final int PORT = 1024;
+
 	public static void main(String[] args) throws IOException {
-		
+
 		int clientCount = 0;
-		
-		ServerSocket serverSocket = new ServerSocket(1024);
-		while(true) {
+		int ClientId = 0;
+
+		@SuppressWarnings("resource")
+		ServerSocket serverSocket = new ServerSocket(PORT);
+		while (true) {
 			Socket socket = null;
 			try {
 				socket = serverSocket.accept();
 				System.out.println("Client Count: " + ++clientCount);
+				ClientId = 1000 + clientCount;
 				DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
 				DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
 				System.out.println("Threaded Client #" + clientCount);
-				Thread thread = new ClientHandler(socket, dataInputStream, dataOutputStream);
+				Thread thread = new ClientHandler(socket, dataInputStream, dataOutputStream, ClientId);
 				thread.start();
-			}catch (Exception e) {
-				socket.close();
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
 }
